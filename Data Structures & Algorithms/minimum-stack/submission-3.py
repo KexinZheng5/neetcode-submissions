0d433fc16332ce4dd51560049554e8d0cc7328fa
Kexin Zheng <kexinz0227@gmail.com>
2026-07-06 20:35:13 -0700

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.py
@@ -0,0 +1,34 @@
+class MinStack:
+
+    def __init__(self):
+        self.min = 0
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.min = val
+            self.stack.append(0)
+        else:
+            self.stack.append(val - self.min)
+            if val < self.min:
+                self.min = val
+
+    def pop(self) -> None:
+        if not self.stack:
+            return
+        
+        val = self.stack.pop()
+        
+        if val < 0:
+            self.min = self.min - val
+
+    def top(self) -> int:
+        top = self.stack[-1]
+
+        if top > 0:
+            return top + self.min
+        else:
+            return self.min
+
+    def getMin(self) -> int:
+        return self.min
