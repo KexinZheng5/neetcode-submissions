900f5b4e571a0e4f0e2832f433eebd4faad469e2
Kexin Zheng <kexinz0227@gmail.com>
2026-09-08 16:31:08 -0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,15 @@
+from collections import Counter
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = {}
+        for s in strs:
+            # get string character count
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            count = tuple(count)
+            if count in d:
+                d[count].append(s)
+            else:
+                d[count] = [s]
+        return list(d.values())
